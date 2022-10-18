9c77fbc9ef151c619272e7642ebbfb96d6ba4635
Pavel Uspensky <sitetobe@yandex.ru>
2022-10-18 17:43:04 +0300

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,10 @@
+{
+
+    let galImg = document.querySelectorAll('.media__img');
+    for (let i = 0; i < galImg.length; i++) {
+        galImg[i].addEventListener('click', function() {
+            galImg[i].classList.toggle('clicked');
+        });
+    }
+
+}
